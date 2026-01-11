ab4ef4f94882e17f91c93ef73b74ae2f33ac003a
Kshitij Choudhary <kshitijchoudhary18@gmail.com>
2026-01-11 17:40:18 +0530

    both parallax completed with and sidebar is fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import "./app.scss"
 import Hero from "./componentes/hero/Hero";
 import Navbar from "./componentes/navbar/navbar";
 import Test from "./componentes/Test";
+import Parallax from "./componentes/parallax/Parallax";
 
 const App=()=>{
  return (
   <div>
     <section id="Homepage" > <Navbar/>
     <Hero/> </section>
-    <section id="Services">Parallax</section>
+    <section id="Services"><Parallax type="services"/></section>
     <section>Serices</section>
-    <section id="Portfolio">Parallax</section>
+    <section id="Portfolio"><Parallax type="portfolio"/></section>
     <section>Portfolio1</section>
     <section>portfolio2</section>
     <section>portfolio3</section>
