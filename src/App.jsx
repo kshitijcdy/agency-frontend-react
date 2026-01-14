5e312d66ac5aeaaf90e6c7902a6e5783376b6985
Kshitij Choudhary <kshitijchoudhary18@gmail.com>
2026-01-15 00:13:57 +0530

    fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import "./App.scss";
-import Hero from "./componentes/hero/Hero";
-import Navbar from "./componentes/navbar/navbar";
-import Test from "./componentes/Test";
-import Parallax from "./componentes/parallax/Parallax";
-import Services from "./componentes/services/Services";
-import Portfolio from "./componentes/portfolio/Portfolio";
-import { Contact } from "./componentes/contact/Contact";
-import Cursor from "./componentes/cursor/Cursor";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/navbar";
+import Parallax from "./components/parallax/Parallax";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import { Contact } from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
 
 const App = () => {
   return (
@@ -21,14 +20,16 @@ const App = () => {
       </section>
       <section>
         <Services />
-      </section> 
+      </section>
       <section id="Portfolio">
         <Parallax type="portfolio" />
       </section>
       <Portfolio />
-      <section id="Contact"><Contact/></section>
+      <section id="Contact">
+        <Contact />
+      </section>
     </div>
-    // <Test/>
+    
   );
 };
 
